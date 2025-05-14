37c3b5f54a412a17b7045e8c2a00693014cbea83
Tao Su <96sutao@gmail.com>
2025-05-13 20:23:22 -0400

    add home page to package.json

--- my-switch-reactApp/vite.config.js
+++ my-switch-reactApp/vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/switch-2-timer-react/', // ← must match the repo name!
   plugins: [react()],
 })
